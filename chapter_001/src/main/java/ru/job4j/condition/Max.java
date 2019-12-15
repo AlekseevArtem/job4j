ee12b49fc41bf1bae4f67c5b822431e6e58fb721
AlekseevArtem <tema9991@mail.ru>
2019-12-15 12:15:23 +0300

    4.2. Максимум из двух чисел[#206809] Внесение изменений на основе комментария Andrei Hincu

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 }
 
