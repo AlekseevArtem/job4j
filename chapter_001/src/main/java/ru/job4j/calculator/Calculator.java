a2b5703b051d3beff202712318d613a0f53d4cea
AlekseevArtem <tema9991@mail.ru>
2019-12-18 13:32:48 +0300

    5.8. JavaDoc. Code convention[#206879]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,51 @@
 package ru.job4j.calculator;
 
+/**
+ * Class для вычисления арифметических операций + = * /.
+ * @author AlekseevArtem (tema9991@mail.ru)
+ * @since  08.12.2019
+ * @version  1
+ */
 public class Calculator {
-
+    /**
+     * Сложение.
+     * @param  first первый аргумент
+     * @param  second первый аргумент
+     * @return  result результат
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
 
+    /**
+     * Деление.
+     * @param  first первый аргумент
+     * @param  second первый аргумент
+     * @return  result результат
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
 
+    /**
+     * Умножение.
+     * @param  first первый аргумент
+     * @param  second первый аргумент
+     * @return  result результат
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
 
+    /**
+     * Вычитание.
+     * @param  first первый аргумент
+     * @param  second первый аргумент
+     * @return  result результат
+     */
     public static void substract(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author AlekseevArtem (tema9991@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+package ru.job4j.calculator;
