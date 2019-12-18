11ea123ae73d0cfc2cb47f55fd4ce246bc01a40b
AlekseevArtem <tema9991@mail.ru>
2019-12-18 16:34:50 +0300

    6.1. Классический поиск перебором.[#206815]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ * Class находит значение в массиве и возвращает позицию первого попавшегося значения.
+ * @author AlekseevArtem (tema9991@mail.ru)
+ * @since  18.12.2019
+ * @version  1
+ */
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = 0 ; index < data.length; index++ ) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas6Then5() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3, 4, 3, 6, 6, 6, 6};
+        int value = 6;
+        int result = find.indexOf(input, value);
+        int expect = 5;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas2ThenMinus1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3, 4, 3, 6, 4};
+        int value = 2;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
