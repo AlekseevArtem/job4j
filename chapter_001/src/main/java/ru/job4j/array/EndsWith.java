1699322df527df5c11b0b9c197948ee03c08f2e9
AlekseevArtem <tema9991@mail.ru>
2019-12-19 14:09:54 +0300

    6.4.1. Слово заканчивается с ...[#206821]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * Class Проверяет равны ли последние ячейки массива с ячейками второго массива.
+ * @author AlekseevArtem (tema9991@mail.ru)
+ * @since  19.12.2019
+ * @version  1
+ */
+public class EndsWith {
+
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        int numw = word.length-1;
+        int nump = post.length-1;
+        for ( ; nump > 0 ;numw--, nump--) {
+            if (word[numw] != post[nump]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
