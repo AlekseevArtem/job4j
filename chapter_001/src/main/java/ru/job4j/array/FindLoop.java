d93765a03dbf2a823ccc795d79b20bb68fb3a59c
AlekseevArtem <tema9991@mail.ru>
2019-12-19 14:24:10 +0300

    6.5. Поиск индекса в диапазоне.[#206822]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -7,9 +7,20 @@ package ru.job4j.array;
  * @version  1
  */
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0 ; index < data.length; index++ ) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static int indeinrangeOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start ; index < finish; index++ ) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -36,4 +36,37 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indeinrangeOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindMinusOne() {
+        int[] input = new int[] {5, 2, 10, 2, 4, 2, 10, 7, 4};
+        int value = 7;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indeinrangeOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind7() {
+        int[] input = new int[] {5, 2, 10, 2, 4, 2, 10, 7, 4};
+        int value = 7;
+        int start = 3;
+        int finish = 8;
+        int result = FindLoop.indeinrangeOf(input, value, start, finish);
+        int expect = 7;
+        assertThat(result, is(expect));
+    }
 }
