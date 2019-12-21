41bceda3b0908288b180bb06a26c695b32b80aa9
AlekseevArtem <tema9991@mail.ru>
2019-12-21 19:42:46 +0300

    6.5.3. Сортировка выборкой.[#206823]

---
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * Class Сортирует любой массив чисел по возрастанию.
+ * @author AlekseevArtem (tema9991@mail.ru)
+ * @since  21.12.2019
+ * @version  1
+ */
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        for (int start = 0 ; start < data.length; start ++){
+            int min = MinDiapason.findMin(data, start, data.length);
+            int index = FindLoop.indeinrangeOf(data, min, start, data.length);
+
+            if (index != start){
+            int temporary = data[start];
+            data[start] = data [index];
+            data[index] = temporary;
+            }
+        }
+        return data;
+    }
+}
