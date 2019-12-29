b2052ea80001da1c5b2b8b615ab98d3074158d9b
AlekseevArtem <tema9991@mail.ru>
2019-12-30 01:34:21 +0300

    1.5. Поля объекта[#206959]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.print(this.name);
+        System.out.print(" ate ");
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
