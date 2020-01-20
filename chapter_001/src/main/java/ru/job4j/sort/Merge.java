db8d27b7f64468dd8c08eee0be03a39a6a54b584
AlekseevArtem <tema9991@mail.ru>
2020-01-20 21:14:38 +0300

    1. Экзамен. Базовый синтаксис.[#206866]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -6,36 +6,21 @@ public class Merge {
 
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int il = 0, ir = 0, irsl = 0;
-        while (irsl < rsl.length && il <= left.length && ir <= right.length) {
-            if (right.length == 0 && left.length == 0) {
-                break;
-            } else if (il == left.length) {
-                rsl[irsl] = right[ir];
+        int il = 0, ir = 0;
+        while (ir + il < rsl.length && il <= left.length && ir <= right.length) {
+            if (il == left.length) {
+                rsl[ir + il] = right[ir];
                 ir++;
-                irsl++;
             } else if (ir == right.length) {
-                rsl[irsl] = left[il];
+                rsl[ir + il] = left[il];
                 il++;
-                irsl++;
             } else if (left[il] < right[ir] && left.length - 1 >= il) {
-                rsl[irsl] = left[il];
+                rsl[ir + il] = left[il];
                 il++;
-                irsl++;
-            } else if (left[il] > right[ir] && right.length - 1 >= ir) {
-                rsl[irsl] = right[ir];
-                ir++;
-                irsl++;
             } else {
-                rsl[irsl] = left[il];
-                if (right.length - 1 >= ir) {
-                    ir++;
-                } else if (left.length - 1 >= il) {
-                    il++;
-                }
-                irsl++;
+                rsl[ir + il] = right[ir];
+                ir++;
             }
-
         }
         return rsl;
     }
