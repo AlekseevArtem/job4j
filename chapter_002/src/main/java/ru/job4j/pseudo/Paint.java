8e13dc5a01291ca1f8c14ff5418cd45d1907c678
AlekseevArtem <tema9991@mail.ru>
2020-01-31 17:23:39 +0300

    7. Шаблон проектирования - Стратегия.[#206942]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,12 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Artem Alekseev (tema9991@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    public void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.pseudo;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Artem Alekseev (tema9991@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PaintTest {
+    @Test
+    public void whenDrawSquare() {
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфур для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+        new Paint().draw(new Square());
+        // проверяем результат вычисления
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("++++")
+                                .append("+     +")
+                                .append("+     +")
+                                .append("++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("  +  ")
+                                .append(" + + ")
+                                .append("+   +")
+                                .append("+++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+        System.setOut(stdout);
+    }
+}
