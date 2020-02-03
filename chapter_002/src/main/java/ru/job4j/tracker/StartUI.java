fe442743791cbafa2bd9a7e47ddebf92d5b0a442
AlekseevArtem <tema9991@mail.ru>
2020-02-03 19:49:36 +0300

    3. if-else-throw[#206946]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -20,7 +20,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Input input = new ConsoleInput();
-        Input validate = new ValidateInput(input);
+        ValidateInput validate = new ValidateInput(input);
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(),
