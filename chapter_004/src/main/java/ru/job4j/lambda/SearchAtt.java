647ad0d368a70f54ac1f631c42370a20e27a11d5
AlekseevArtem <tema9991@mail.ru>
2020-03-06 12:37:00 +0300

    2.0. Встроенные функциональные интерфейсы. [#249209] Внес изменения на основе комментариев Rail Shamsemuhametov

--- chapter_004/src/main/java/ru/job4j/lambda/SearchAtt.java
+++ chapter_004/src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -2,14 +2,13 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiPredicate;
-import java.util.function.Supplier;
+import java.util.function.Predicate;
 
 public class SearchAtt {
-    private static List<Attachment> filter(List<Attachment> to, BiPredicate func, Supplier initValue) {
+    private static List<Attachment> filter(List<Attachment> to, Predicate<Attachment> func) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : to) {
-            if (func.test(att, initValue.get())) {
+            if (func.test(att)) {
                 rsl.add(att);
             }
         }
@@ -17,35 +16,11 @@ public class SearchAtt {
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        BiPredicate<Attachment, Integer> func = new BiPredicate<>() {
-            @Override
-            public boolean test(Attachment o, Integer o2) {
-                return o.getSize() > o2;
-            }
-        };
-        Supplier<Integer> initValue = new Supplier<>() {
-            @Override
-            public Integer get() {
-                return 100;
-            }
-        };
-        return filter(list, func, initValue);
+        return filter(list, o -> o.getSize() > 100);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        BiPredicate<Attachment, String> func = new BiPredicate<>() {
-            @Override
-            public boolean test(Attachment o, String o2) {
-                return o.getName().contains(o2);
-            }
-        };
-        Supplier<String> initValue = new Supplier<String>() {
-            @Override
-            public String get() {
-                return "bug";
-            }
-        };
-        return filter(list, func, initValue);
+        return filter(list, o -> o.getName().contains("bug"));
     }
 }
 
