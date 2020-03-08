763c940e50deabcf455e669176b445b24fd303d3
AlekseevArtem <tema9991@mail.ru>
2020-03-08 20:18:20 +0300

    2. Local-Variable Type Inference[#206790]

--- chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -9,11 +9,11 @@ import static org.junit.Assert.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 }
--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -12,15 +12,15 @@ import static org.junit.Assert.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
-        Task result1 = queue.take();
+        var result1 = queue.take();
         assertThat(result1.getDesc(), is("middle"));
-        Task result2 = queue.take();
+        var result2 = queue.take();
         assertThat(result2.getDesc(), is("low"));
     }
 }
